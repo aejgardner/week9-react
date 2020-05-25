eee463c530e3f6e2676f3df1e0b830e4d7864d5e
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-25 14:06:27 +0100

    Stuff component added

--- week-9-app/src/App.js
+++ week-9-app/src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header";
-import Paragraph from "./components/Paragraph";
-import Square from "./components/Square";
-import People from "./components/People";
+import Stuff from "./components/Stuff";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <People />
-        <Square />
-        <Paragraph />
+        <Stuff />
       </header>
     </div>
   );
