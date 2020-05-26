16123f4caf385ca74dccfb93575e773f8ea23f4d
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-26 10:56:15 +0100

    Header component updated

--- week-9-app/src/App.js
+++ week-9-app/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 import Stuff from "./components/Stuff";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header text="Hello" />
         <Stuff />
       </header>
     </div>
