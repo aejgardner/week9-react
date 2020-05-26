d8984a14f760e63b2e7646e1203866fd779d870c
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-26 12:26:28 +0100

    classcomponents folder added for class component challenges

--- week-9-app/src/App.js
+++ week-9-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Stuff from "./components/Stuff";
+import Clicked from "./classcomponents/Clicked";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Stuff />
+        <Clicked />
       </header>
     </div>
   );
