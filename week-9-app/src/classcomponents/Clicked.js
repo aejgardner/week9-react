2145f4643e9ef13a29fa49e2488cea51582202cf
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-26 12:41:42 +0100

    Clicked class component added

---
+++ week-9-app/src/classcomponents/Clicked.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+class Clicked extends Component {
+    constructor(props) {
+            super(props);
+        
+        this.state = {
+            clicked: false
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+
+        this.setState({ clicked: !this.state.clicked });
+    }
+
+    render() {
+        return (
+            <p onClick={ this.handleClick }>{ this.state.clicked ? "Not clicked" : "Clicked" }</p>
+        )
+    }
+}
+
+export default Clicked;
