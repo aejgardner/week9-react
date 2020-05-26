3701984bfd01987a238a1d713895a3f9fa6e4306
Adam Gardner <adam.gardner4991@gmail.com>
2020-05-26 12:18:18 +0100

    Header changed to class component

--- week-9-app/src/components/Header.js
+++ week-9-app/src/components/Header.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Header = ({children}) => (
-    <h1>{children}</h1>
-)
+class Header extends Component {
+
+    render() {
+        const { children } = this.props;
+        return (
+            <h1>{children}</h1>
+        )
+    }
+}
 
 export default Header;
